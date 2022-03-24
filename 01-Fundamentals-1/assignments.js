a161dc3a9868dbdd8cb591a9498c9c306218777c
Vinh Mao <vinhxmao87@gmail.com>
2022-03-23 23:51:34 -0700

    data types, variable naming review

---
+++ 01-Fundamentals-1/assignments.js
@@ -0,0 +1,19 @@
+// Values and Variables
+let country = "United Sates";
+let continent = "North America";
+let population = 329500000;
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+// Data Types
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// let, const and var
--- 01-Fundamentals-1/script.js
+++ 01-Fundamentals-1/script.js
@@ -6,3 +6,42 @@ if (js === 'amazing') alert("JavaScript is fun!");
 
 // But THIS WILL show up on the console!
 console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23)
+
+// Declaring variables and using them
+let firstName = "Jonas";
+console.log(firstName + " Bob")
+
+// Variable naming conventions:
+// Camel-cased always for JavaScript! The first letter of a word is lower-cased and every 
+// other letter of separate words gets upper-cased instead.
+// Underscored is standard in other programming languages.
+// Don't start variable names with an upper-case letter, unless all letters in the
+// variable name are capitalized.
+//
+// But as usual you cannot start variable names with numbers 
+// or symbols, or you get a SyntaxError. Only letters, underscores and
+// $ are allowed. Reserved keywords cannot be variable names either.
+//
+// Variable names must also be descriptive too. You should know what a variable is
+// just by looking at its name.
+let myFirstJob = "Tutor";
+let myCurrentJob = "Engineer";
+
+// This is bad convention:
+let job1 = "Tutor";
+let job2 = "Engineer";
+
+// Data Types
+// Number: decimals/integers
+// String: sequence of characters
+// Boolean: true/false
+// Undefined: value taken by a variable that is not yet defined
+// Null: 'empty value'
+// Symbol: Value that is unique and can't be changed
+// Bigint: Larger integers than the Number type can hold
+//
+// Dynamic typing: data types are determined automatically. You
+// dob't have to define them immediately.
