f2981b604ee6b282214206328a1eb7837ec9b036
Vinh Mao <vinhxmao87@gmail.com>
2022-03-29 00:51:41 -0700

    Strict Mode applications added

--- 02-Fundamentals-2/script.js
+++ 02-Fundamentals-2/script.js
@@ -1,3 +1,15 @@
 // Activates Strict Mode in the entire script; must be the first line in file!
-// We can avoid accidental errors using this line of code below.
+// We can avoid accidental errors and bugs using this line of code below.
 'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive!!!");
+
+// // With Strict Mode on, words like 'if', 'interface' and 'private' will
+// // not be allowed since they may be future words to be implemented in 
+// // future JavaScript updates.
+// const interface = 'Audio';
+// const private = 534;
