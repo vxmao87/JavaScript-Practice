05e46d8e4e357ab21f90412da07f420467981b26
Vinh Mao <vinhxmao87@gmail.com>
2022-03-30 00:30:42 -0700

    function review

---
+++ 02-Fundamentals-2/assignments.js
@@ -0,0 +1,10 @@
+// Functions
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+}
+
+console.log(describeCountry("The United States", 330, "Washington D.C."));
+console.log(describeCountry("China", 1402, "Beijing"));
+console.log(describeCountry("Japan", 126, "Tokyo"));
+
+// Function Declarations VS Expressions
--- 02-Fundamentals-2/script.js
+++ 02-Fundamentals-2/script.js
@@ -2,14 +2,47 @@
 // We can avoid accidental errors and bugs using this line of code below.
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+/*----------------------------------------------------------------------------------*/
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive!!!");
+// Strict Mode
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive!!!");
 
 // // With Strict Mode on, words like 'if', 'interface' and 'private' will
 // // not be allowed since they may be future words to be implemented in 
 // // future JavaScript updates.
 // const interface = 'Audio';
 // const private = 534;
+
+/*----------------------------------------------------------------------------------*/
+
+/*
+
+Functions
+
+They are stacks of code that can be used over and over again, perfect for running 
+test cases in particular! Functions can come with values called parameters. You can call,
+run or invoke a function, all mean the same thing.
+
+Functions can also return values!
+
+*/
+
+function logger() {
+  console.log("My name is Jonas!");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  return `Juice with ${apples} apples and ${oranges} oranges.`;;
+}
+
+console.log(fruitProcessor(5, 0));
