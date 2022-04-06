5f997bbac35836731de4d906cb1c1e64560bd9db
Vinh Mao <vinhxmao87@gmail.com>
2022-04-06 00:51:02 -0700

    developer skills section added

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,39 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
+
+Prettier and VS Code
+
+Prettier will give us an opinionated format of how our code should look. It is an
+opinionated code formatter, and it may not be for everyone. To enable or disable,
+go to Preferences => Settings, then search for "Default Formatter" and make
+"Prettier" your default. Set it to "None" to disable it.
+
+You might not like some features, so you can change that! Look at the Prettier docs
+and then create a .prettierrc file in your project. Then set the values you want 
+based on your preferences, like "singleQuote": true.
+
+You may also create code snippets for yourself, by going to Code => Preferences
+=> User Snippets, and clicking on New Global Snippets file. Then create it as
+desired. Just type the prefix you have in the file and it'll work!
+
+Some extensions to consider:
+
+ESLint
+Auto Close Tag
+Auto Rename Tag
+image preview by Kiss Tamas
+Monokai Pro
+Prettier
+Settings Sync
+TODO Highlight
+
+*/
+
+// const x = 23;
+// if (x === 23) console.log(23);
+
+// const calcAge = birthYear => 2037 - birthYear;
+
+/*-----------------------------------------------------------------------------------------*/
