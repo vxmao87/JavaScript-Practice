5722b0b12dd9811126dac194768ace881b77dd38
Vinh Mao <vinhxmao87@gmail.com>
2022-05-02 00:22:23 -0700

    pig game DOM project in progress

--- 05-The-DOM-and-Events/02-Modal/script.js
+++ 05-The-DOM-and-Events/02-Modal/script.js
@@ -1,1 +1,60 @@
-'use strict';
+"use strict";
+
+/*
+
+Notes
+
+To kill a command through the Terminal, type Ctrl + C on the Mac.
+
+document.querySelector selects only the first instance fo the element. So use 
+querySelectorAll instead. The elements are all stored in a list to loop over.
+
+We will use the remove() method to remove classes from an HTML tag.
+You could also do modal.style.displayed = "block"; as an alternative.
+
+But in general, we use modal.classList.add().
+
+We also have keyboard events! We use addEventListener, but instead of "click",
+we use . And these are also global events occurring anywhere in the document.
+
+There are the keydown, keypress and keyup events for keys. You'll just need to pass in
+an 'event' parameter inside your event handler function.
+
+*/
+
+// Selects the elements needed by class.
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsShowModal = document.querySelectorAll(".show-modal");
+
+// Opens the modal by removing the "hidden" tags from the elements.
+const openModal = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// Closes the modal by adding back the "hidden" tags from the elements.
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// Adds an event listener to every button on the screen.
+for (const element of btnsShowModal) {
+  element.addEventListener("click", openModal);
+}
+
+// Adds an event listener to close the modal when the "x" is clicked on.
+btnCloseModal.addEventListener("click", closeModal);
+
+// Adds an event listener to the overlay that closes the modal when it is clicked on.
+overlay.addEventListener("click", closeModal);
+
+// Adds an event listener to close the modal when the user presses the Escape key.
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
--- 05-The-DOM-and-Events/03-Pig-Game/script.js
+++ 05-The-DOM-and-Events/03-Pig-Game/script.js
@@ -1,1 +1,51 @@
-'use strict';
+"use strict";
+
+// Selects elements we need for this project.
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// Sets all starting conditions.
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+// Final scores for both players
+const scores = [0, 0];
+
+// Current score for active player
+let currentScore = 0;
+
+// The active player (0 or 1)
+let activePlayer = 0;
+
+// Rolls the dice and displays the number that was rolled.
+btnRoll.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+
+  // Adds points to the current player if a 1 is not rolled. Otherwise,
+  // the game moves onto the next player.
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+
+    // Use toggle to change which player is active
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+  }
+});
